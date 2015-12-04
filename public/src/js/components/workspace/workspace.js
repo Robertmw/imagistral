b9b396b4ab149cf903c8b02c19294953e2925697
Robert Pamfile <robert.pamfile@endava.com>
2015-12-04 18:06:34 +0200

    Installing Baobab

--- public/src/js/components/workspace/workspace.js
+++ public/src/js/components/workspace/workspace.js
@@ -1,47 +1,47 @@
 var React = require('react'),
-    ReactDOM = require('react-dom'),
-    BaseComponent = require('../base-component/base-component');
+		ReactDOM = require('react-dom'),
+		BaseComponent = require('../base-component/base-component');
 
 const displayName = 'Workspace';
 
 class Workspace extends BaseComponent {
 
-  constructor (props) {
-    super(props);
-  }
+	constructor (props) {
+		super(props);
+	}
 
-  render() {
-    return (
-      <section className="workspace">
-        <section className="workspace-settings-bar">
-          <div className="toggle-settings">
-            <span className="fa fa-angle-double-right"></span>
-          </div>
-          <div className="zoom-settings">
-            <span className="fa fa-minus"></span>
-            <span className="fa fa-lg fa-search"></span>
-            <span className="fa fa-plus"></span>
-            <div className="custom-select">
-              <label className="current-zoom">
-                100%
-                <span className="fa fa-caret-down"></span>
-              </label>
-            </div>
-          </div>
-          <div className="canvas-settings">
-            <span className="fa fa-rotate-left"></span>
-            <span className="fa fa-rotate-right"></span>
-          </div>
-          <div className="delete">
-            <span className="fa fa-trash"></span>
-          </div>
-        </section>
-        <canvas className="canvas-sheet"></canvas>
-        <div className="scrollbar horizontal"><span className="cursor"></span></div>
-        <div className="scrollbar vertical"><span className="cursor"></span></div>
-      </section>
-    );
-  }
+	render() {
+		return (
+			<section className="workspace">
+				<section className="workspace-settings-bar">
+					<div className="toggle-settings">
+						<span className="fa fa-angle-double-right"></span>
+					</div>
+					<div className="zoom-settings">
+						<span className="fa fa-minus"></span>
+						<span className="fa fa-lg fa-search"></span>
+						<span className="fa fa-plus"></span>
+						<div className="custom-select">
+							<label className="current-zoom">
+								100%
+								<span className="fa fa-caret-down"></span>
+							</label>
+						</div>
+					</div>
+					<div className="canvas-settings">
+						<span className="fa fa-rotate-left"></span>
+						<span className="fa fa-rotate-right"></span>
+					</div>
+					<div className="delete">
+						<span className="fa fa-trash"></span>
+					</div>
+				</section>
+				<canvas className="canvas-sheet"></canvas>
+				<div className="scrollbar horizontal"><span className="cursor"></span></div>
+				<div className="scrollbar vertical"><span className="cursor"></span></div>
+			</section>
+		);
+	}
 
 }
 
