a988c569362c33bee62325111b5acf0a2b8c7fe3
Vlad Bicu <vlad.bicu@cynny.com>
2016-01-05 16:52:44 +0200

    importImage.js: initial import image engine

---
+++ public/src/js/engine/importImage.js
@@ -0,0 +1,17 @@
+import fabricjs from '../../../../bower_components/fabric.js/dist/fabric.min.js';
+import * as engine from './engine';
+
+export function addImage(url){
+	if (engine.canvas !== null) {
+		fabricjs.fabric.Image.fromURL(url, (myImage) => {
+			engine.canvas.add(myImage).setActiveObject(myImage);
+			engine.canvas.getActiveObject().selectable = true;
+		});
+	} else {
+		// TODO wait until issue #52 its resolved
+		// fabricjs.fabric.Image.fromURL(url, (myImage) => {
+		// 	engine.engineInit(myImage.width, myImage.height);
+		// 	engine.canvas.add(myImage);
+		// });
+	}
+}
