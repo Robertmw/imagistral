eff1f751d5c5663e0664235e449c78a10c911b57
Robert Pamfile <robert.pamfile@endava.com>
2016-01-26 17:01:48 +0200

    drop some tools

--- public/src/js/state.js
+++ public/src/js/state.js
@@ -40,12 +40,6 @@ const tree = new Baobab({
 			subTools: []
 		},
 		{
-			name: 'crop',
-			icon: 'icon-crop',
-			caret: '',
-			subTools: []
-		},
-		{
 			name: 'brush',
 			icon: 'icon-brush',
 			caret: '',
@@ -85,12 +79,6 @@ const tree = new Baobab({
 			name: 'filters',
 			icon: 'icon-filters',
 			caret: '',
-			subTools: []
-		},
-		{
-			name: 'zoom',
-			icon: 'icon-magnifier',
-			caret: '', /*'fa fa-caret-down caret',*/
 			subTools: []
 		}
 	],
